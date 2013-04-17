ae38ae5b985fe6f685bef614caa78fc7e9b99d9c
Vinay <vinay@vinay-HP-ProBook-4510s.(none)>
2013-04-17 20:19:12 +0530

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App1::Application.config.secret_key_base = '9b21a679d3653ebbb75cd68b0ca989ce75a3b949c254f39c8d4e65b4cdb18480fbf59d816a63f318eb4737ea93570855c4ebd8e49baf93fbeb14b580400f5228'
